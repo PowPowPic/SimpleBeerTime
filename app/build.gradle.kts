17e85af29d855f805afc5187e8120ea8e21af1ac
PowPowPic <powder.power.pictures@gmail.com>
2026-03-03 13:44:37 +0900

    SBT39 ver34 1.7.1 本番リリース

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.powder.simplebeertime"
         minSdk = 24
         targetSdk = 36
-        versionCode = 32
-        versionName = "1.7.0"
+        versionCode = 34
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
